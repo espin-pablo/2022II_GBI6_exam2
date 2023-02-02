84bf91ece908d5401aa48d55bcab3988c9ce2c61
espin-pablo <117690533+espin-pablo@users.noreply.github.com>
2023-02-01 20:27:00 -0500

    Subida Archivos examen
    Cambios realizados por pablooo

--- coati.py
+++ coati.py
@@ -1,9 +1,16 @@
 from Bio import Entrez 
 from Bio import SeqIO
-import re
-import csv
+from Bio.Seq import Seq 
+from Bio.SeqRecord import SeqRecord 
+from Bio import SeqIO 
 
 def fasta_downloader():
-        id_coati = open("data\coati.txt", "r")
-        print(id_coati.readlines())
-
+    """" 
+    Permite obtener un archivo gb
+    """   
+    with open('data\coati.txt', 'r+') as a:
+            id_coati = a.readlines()    
+    Entrez.email = "gualapuro.moises@gmail.com"
+    with Entrez.efetch( db="nucleotide", rettype="gb", retmode="text", id= "id_coati") as handle:
+        seq_record = SeqIO.read(handle, "gb")
+        SeqIO.write(seq_record, "data\coati.gb", "gb")
